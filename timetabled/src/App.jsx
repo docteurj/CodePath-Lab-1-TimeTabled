3a5f6e68451ae702e96a6c70187af70dd155b7ae
docteurj <jonathandocteur@gmail.com>
2023-02-21 20:20:49 -0500

    Added Time + Days of Week

--- timetabled/src/App.jsx
+++ timetabled/src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css';
+import Calendar from './components/Calendar'
+
 
 const App = () => {
 
   return (
     <div className="App">
       <h1>Itinerary for 7 Days in Hawaii!🏖️</h1>
-      <h2>Hawaii is a beautiful island. 7 Days is perfect for a trip to Hawaii. Use this to plan accordingly!
-      </h2>
+      <h2>Hawaii is a beautiful island. 7 Days is perfect for a trip to Hawaii. Use this to plan accordingly! </h2>
+      <Calendar/>
       
     </div>
   )
---
+++ timetabled/src/components/Calendar.jsx
@@ -0,0 +1,126 @@
+import React from "react";
+
+const Calendar = () => {
+  return (
+    <table>
+    <thead>
+         <tr>
+            <th></th>
+            <th>Sunday</th>
+            <th>Monday</th>
+            <th>Tuesday</th>
+            <th>Wednesday</th>
+            <th>Thursday</th>
+            <th>Friday</th>
+            <th>Saturday</th>
+        </tr>         
+    </thead>
+    <tbody>
+      <tr>
+          <td className="time">8AM</td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+      </tr>
+      <tr>
+          <td className="time">9AM</td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+      </tr>
+      <tr>
+          <td className="time">10AM</td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+      </tr>
+      <tr>
+          <td className="time">11AM</td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+      </tr>
+      <tr>
+          <td className="time">12PM</td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+      </tr>
+      <tr>
+          <td className="time">1PM</td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+      </tr>
+      <tr>
+          <td className="time">2PM</td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+      </tr>
+      <tr>
+          <td className="time">3PM</td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+      </tr>
+      <tr>
+          <td className="time">4PM</td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+      </tr>
+      <tr>
+          <td className="time">5PM</td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+      </tr>
+    </tbody>
+  </table>
+
+  );
+};
+
+
+export default Calendar;
